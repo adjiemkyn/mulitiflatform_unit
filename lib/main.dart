187f502ca2ca3a40e49b47175887217aabec3dc2
adjiemkyn <adjiemikayana@gmail.com>
2024-03-06 19:56:05 +0800

    column

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,7 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Praktikum Layout Flutter'),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: Column(
           children: <Widget>[
             Container(color: Colors.red, height: 100, width: 100),
             Container(color: Colors.green, height: 50, width: 100),
