cd301c24f9291c3cb24e62bc6aa4c573d2d7cf7f
adjiemkyn <adjiemikayana@gmail.com>
2024-03-06 19:39:41 +0800

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,17 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   @override
-  
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Praktikum Layout Flutter'),
+        ),
+        body: Row(
+          children: <Widget>[
+            Container(color: Colors.red, height: 100, width: 100),
+            Container(color: Colors.green, height: 100, width: 100),
+            Container(color: Colors.blue, height: 100, width: 100),
+          ],
+        ));
+  }
 }
-
-class 
