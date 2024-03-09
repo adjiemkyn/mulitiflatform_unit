6348c525a911c4fc443fa1c187eb33c9ba2ea1f4
adjiemkyn <adjiemikayana@gmail.com>
2024-03-09 15:33:43 +0800

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Praktikum Layout Flutter'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(children: <Widget>[
-            Container(color: Colors.red, height: 500, width: 100),
-            Container(color: Colors.green, height: 500, width: 50),
-            Container(color: Colors.blue, height: 500, width: 80),
-          ]),
-        ));
+      appBar: AppBar(
+        title: Text('Praktikum Layout Flutter'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(color: Colors.red, height: 500, width: 100),
+          Container(color: Colors.green, height: 500, width: 50),
+          Container(color: Colors.blue, height: 500, width: 80),
+        ],
+      ),
+    );
   }
 }
